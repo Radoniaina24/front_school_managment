a5f41d924fae55311d046970f8c9cc9a240d8cc2
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2024-10-20 12:22:12 +0300

    vv1.2

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -4,16 +4,22 @@ import Students_Svg from "./Svg/students";
 import Teachers_Svg from "./Svg/Teachers";
 import Parents_Svg from "./Svg/Parents";
 import Earnings_Svg from "./Svg/Earnings";
+import Link from "next/link";
 
 export default function Dashboard() {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-      <CardDataStats title="Students" total="5000">
-        <Students_Svg />
-      </CardDataStats>
-      <CardDataStats title="Teachers" total="1500">
-        <Teachers_Svg />
-      </CardDataStats>
+      <Link href={"/student"}>
+        <CardDataStats title="Students" total="5000">
+          <Students_Svg />
+        </CardDataStats>
+      </Link>
+      <Link href={"/teacher"}>
+        <CardDataStats title="Teachers" total="1500">
+          <Teachers_Svg />
+        </CardDataStats>
+      </Link>
+
       <CardDataStats title="Parents" total="6000">
         <Parents_Svg />
       </CardDataStats>
